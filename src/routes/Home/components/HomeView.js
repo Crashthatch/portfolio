3330722faeaecc2aca47ef26e4962afaebffe72c
Tom Fletcher <fletcher.tom@gmail.com>
2017-01-25 13:45:56 +0000

    Move container to its own directory to keep in line with the "fractal" project structure.

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import DuckImage from '../assets/Duck.jpg'
-import ProjectListContainer from './ProjectListContainer'
+import ProjectListContainer from '../containers/ProjectListContainer'
 import './HomeView.scss'
 
 export const HomeView = (props) => {
